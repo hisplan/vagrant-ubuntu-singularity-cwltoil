0c63325a8c60a0cfbcf1049a8ab77f54235da18c
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2017-02-23 07:10:18 -0500

    Initial commit

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update -y
+
+# install python2.7 and pip
+apt-get install -y python
+apt-get install -y python-pip
+pip install --upgrade pip
+
+# install development tools
+apt-get -y install build-essential curl git sudo man vim autoconf automake libtool debootstrap
+
+# install toil+cwl
+pip install toil[cwl]
+
+# install sigularity
+git clone https://github.com/gmkurtzer/singularity.git
+cd singularity
+./autogen.sh
+./configure --prefix=/usr/local
+make
+make install
+
